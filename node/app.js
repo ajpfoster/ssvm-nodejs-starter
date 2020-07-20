a90c08b67810ca8fb6ac1a7a066934ec5f0b02f7
Ash Foster <ajp.foster@gmail.com>
2020-07-21 03:16:23 +1200

    Added all to starter env - should pull out wasm

--- node/app.js
+++ node/app.js
@@ -6,14 +6,14 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
+	  const queryObject = url.parse(req.url,true).query;
+	  if (!queryObject['name']) {
+		      res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+		    } else {
+			        res.end(say(queryObject['name']) + '\n');
+			      }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	  console.log(`Server running at http://${hostname}:${port}/`);
 });
